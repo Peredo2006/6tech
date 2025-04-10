d4e12611a7c3f1a59f1458a7ffcadffcf5cc2b6a
Pedro Santos de Oliveira <117773287+Peredo2006@users.noreply.github.com>
2025-04-10 06:20:27 -0300

    Add files via upload

---
+++ php/banco/6tech.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE contato;
+
+USE contato;
+
+
+CREATE TABLE `tbmensagens` (
+  `nome` varchar(100) DEFAULT NULL,
+  `sobrenome` varchar(100) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `assunto` varchar(100) DEFAULT NULL,
+  `mensagem` varchar(100) DEFAULT NULL,
+  `data_envio` DATETIME DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+SELECT * FROM tbmensagens;
+
+TRUNCATE TABLE tbmensagens;
---
+++ php/mysql/conexao.php
@@ -0,0 +1,9 @@
+<?php 
+    $server = "localhost";
+    $user = "root";
+    $senha = "";
+    $banco = "contato";
+    $conn = mysqli_connect(
+        $server, $user, $senha, $banco
+    );
+?>
