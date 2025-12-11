b86a9699ec17696913db55e09d1c3d751da2e37f
Pedro Santos de Oliveira <117773287+Peredo2006@users.noreply.github.com>
2025-12-11 13:46:57 -0300

    Update conexao.php

--- php/mysql/conexao.php
+++ php/mysql/conexao.php
@@ -1,7 +1,7 @@
 <?php
 $server= "40.160.64.65";
-$user= "ifhostgru_6tech";
+$user= "talentos_6tech";
 $password= "3Y&WkKD)XM~?a,HW";
-$dbname= "ifhostgru_6tech";
+$dbname= "talentos_6tech";
 $conn = mysqli_connect($server, $user, $password, $dbname);
 ?>
