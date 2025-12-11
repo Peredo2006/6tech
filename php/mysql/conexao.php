8a883fbffeb68e9932b0faadbe8c868c1b32cde6
Pedro Santos de Oliveira <117773287+Peredo2006@users.noreply.github.com>
2025-12-11 13:45:50 -0300

    Update database connection password

--- php/mysql/conexao.php
+++ php/mysql/conexao.php
@@ -1,7 +1,7 @@
 <?php
 $server= "40.160.64.65";
 $user= "ifhostgru_6tech";
-$password= "YD3qP-j?Duo5raxZ";
+$password= "3Y&WkKD)XM~?a,HW";
 $dbname= "ifhostgru_6tech";
 $conn = mysqli_connect($server, $user, $password, $dbname);
 ?>
